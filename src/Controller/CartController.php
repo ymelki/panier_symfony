21611e4114f0b7d52d914a047963734d298c9b44
Yoel Melki <ymelki@gmail.com>
2022-04-19 11:19:57 +0200

    first commit

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -9,12 +9,39 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CartController extends AbstractController
 {
+
+
+    // vider le panier 
+    /**
+     * @Route("/cart/clear", name="app_cart_clear")
+     */
+    public function clear(Request $request): Response
+    { 
+        // supprimer la variable cart contenant un tableau enregistré en session
+        $request->getSession()->remove('cart');
+
+        return $this->render('cart/index.html.twig', [
+            'controller_name' => 'CartController',
+        ]);
+
+    }
+
+
     /**
      * @Route("/cart/add/{id}", name="app_cart")
      */
     public function index($id, Request $request): Response
-    {
-        dd($request);
+    { 
+        //Cas N1 : Le panier n'existe pas , la session n'existe pas : je créé la session
+        //Cas N2 : Le panier existe , la session existe : je modifie la session
+        $cart =  $request->getSession()->get('cart' , []);
+
+        // on ecrit dans la session nommé 'cart' la variable $cart contenant []
+        $request->getSession()->set('cart',$cart);
+
+        // affiche la session
+        dd($cart);
+
         // Vardump et Die 
         dd($id);
         return $this->render('cart/index.html.twig', [
